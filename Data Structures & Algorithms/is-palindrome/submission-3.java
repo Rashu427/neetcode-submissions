1d54922b326165c96058932867dc99690eddac73
Rashu <91660322+Rashu427@users.noreply.github.com>
2026-06-17 13:31:52 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char ch:s.toCharArray())
+        {
+            if(Character.isLetterOrDigit(ch))
+            {
+              sb.append (Character.toLowerCase(ch));
+            }
+        }
+        String str=sb.toString();
+        String strReverse=sb.reverse().toString();
+        return str.equals(strReverse);
+        
+    }
+}
